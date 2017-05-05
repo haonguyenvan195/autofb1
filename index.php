da4dc7bf005158500f0f9af87b4e68dba811baf1
haonguyenvan195 <haonguyenvan195@gmail.com>
2017-05-05 18:06:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 echo "abc";
 $token = 'EAAAAAYsX7TsBAFd6FEZBMMlGRXxsDgsoKD4agcCrgcUDXCY8AXNuYYv630aekawNh7yZC0LZCbxp65kEAaeSLrWIAg6Cdmwf3KLDOcdGEyiexNZAYXBSgsBd2jiuo62x3RASBcOqBOytK3ZCcl842LhFKPpltyZAZBvaQy1SVDWKsZAhTSSO54f0VJNIoZAuLWLjl6gHUTZALX6gZDZD'; //
-$limitnf=10; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
+$limitnf=3; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
-$camxuc= array('LIKE');
+$camxuc= array('LOVE');
 $mess=$camxuc[rand(0,count($camxuc)-1)];
 echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
 }
