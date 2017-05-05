111f440e6cd16f2ef8a4ddb644e20fa58d68d9a6
haonguyenvan195 <haonguyenvan195@gmail.com>
2017-05-05 18:39:49 +0700

    Update acc1.php

--- acc1.php
+++ acc1.php
@@ -1,11 +1,11 @@
 <?php
-echo "abc";
-$token = 'EAAAAAYsX7TsBAAdlTjY3fu1KOqGOxt2erBh0cCY3UnFv6CXamYp3HK3mzmbYmGBH9y93ABRL2fC6pJZBq9a4IQ5knt0lW4qyaZCLUaYggbKMd9KmyOr18svCSY3Tu7gLxFQIuZBJZAeLuvGwKQrY7Lh5Mmxj5m1CZCZCVCnPFQxOZCXkzQcVgZCy5sNZC4kJeEKv5BFwjq1unxgZDZD'; //
-$limitnf=3; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
+echo "nguyen an";
+$token = 'EAAAAAYsX7TsBAICJIQiLGD9jQcGUwKEWDuH63ftFI9jHEIW1G9xnE06MjzGmXOYtTnOILu2I1evko5vFDUAJZBlteU6BeZCtLJnHSZClfYARZBSMwmpvvMylEvpa5Y5DNDUZC0hzLEJIMKe8ZBbGCGjqCSjZAgkEGQb7NeZBvt0BHxdEgGZCw4I0w5RAcL9lZAJUFy0v3AljESHANhAgnkx5C2'; //
+$limitnf=5; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
-$camxuc= array('LOVE');
+$camxuc= array('LOVE' , 'LIKE');
 $mess=$camxuc[rand(0,count($camxuc)-1)];
 echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
 }
