6e6a8631a08e00baba7a1af2f0b5ea462a7e59f6
haonguyenvan195 <haonguyenvan195@gmail.com>
2017-05-06 15:48:00 +0700

    Update acc-my.php

--- acc-my.php
+++ acc-my.php
@@ -1,7 +1,7 @@
 <?php
 echo "abc";
-$token = 'EAAAAAYsX7TsBANzMMJuN7tjSKDmKj8I2ZB8V2dqKU8qSnKm8cdjsJSAdZCS1LLHQZBnnuvtIBgnivp4KZBmLuBDydBn7QdNyfWOOZCLFMK96SOtETzlDN6UStCtlMmmmitACXT3x4ysdKH4bGtFRfqbxHXlu3jHSJm1IIlB4uyQUuMxW0GPYLBRfJMsxFRa2APNPUYxE2pIwkuonp9IIx'; //
-$limitnf=5; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
+$token = 'EAAAAAYsX7TsBANcN7FWI4GVgvMXZCW40w3Bt6BfdMIvefzQAPFnB9FhRM47BoKwrmww1bSMuMeyMX9FqVeVAui48dS8YCGZCwW5rZCFufSNKD3YOOZBvIVXpUTlDD3rfxRz7aUASiEzvlxES9UIJ6Rb3ZAfQZBCmerRuCxTaLGKkybyZBwtyZBwDN02f02ElPZCPKtVNQz6lm3gZDZD'; //
+$limitnf=2; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
