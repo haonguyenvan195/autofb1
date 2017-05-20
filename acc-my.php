d9ab75a351911d9637f378561c9082fd41b30b3a
haonguyenvan195 <haonguyenvan195@gmail.com>
2017-05-21 00:17:03 +0700

    Update acc-my.php

--- acc-my.php
+++ acc-my.php
@@ -1,7 +1,7 @@
 <?php
 echo "abc";
 $token = 'EAAAAAYsX7TsBANcN7FWI4GVgvMXZCW40w3Bt6BfdMIvefzQAPFnB9FhRM47BoKwrmww1bSMuMeyMX9FqVeVAui48dS8YCGZCwW5rZCFufSNKD3YOOZBvIVXpUTlDD3rfxRz7aUASiEzvlxES9UIJ6Rb3ZAfQZBCmerRuCxTaLGKkybyZBwtyZBwDN02f02ElPZCPKtVNQz6lm3gZDZD'; //
-$limitnf=5; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
+$limitnf=4; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
